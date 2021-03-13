50dead5d6f62c944501da0a2dd049386ab1ecc39
SergeyZasimov <zasimov.ser@yandex.ru>
2021-03-13 18:03:44 +0300

    homework_5

---
+++ geekshop/basketapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.conf import settings
+
+from mainapp.models import Product
+
+# Create your models here.
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='basket')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(verbose_name='количество', default=0) 
+    add_datetime = models.DateTimeField(auto_now_add=True, verbose_name='время')
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,8 +1,9 @@
 import json
 from django.conf import settings
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from mainapp.models import Product, ProductCategory
+from basketapp.models import Basket
 
 # Create your views here.
 
@@ -26,22 +27,51 @@ def main(request):
 
     return render(request, 'mainapp/index.html', context)
 
+
 def products(request, pk=None):
 
-    products = Product.objects.all()[:4]
+    basket = 0
+    if request.user.is_authenticated:
+        basket = sum(list(Basket.objects.filter(user=request.user).values_list('quantity', flat=True)))
+
+    title =  "продукты"
+
     links_menu = ProductCategory.objects.all()
 
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all().order_by('price')
+            category_item = {'name': 'все', 'pk': 0}
+        else:
+            category_item = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category=category_item)
+
+        content = {
+            'title': title,
+            'links_menu': links_menu,
+            'social_links': social_links,
+            'category': category_item,
+            'products': products,
+            'basket': basket,
+        }
+        
+        return render(request, 'mainapp/products_list.html', content)
+
+
+    products = Product.objects.all()
     context = {
-            'title': "продукты",
+            'title': title,
             'links_menu': links_menu,
             'social_links': social_links,
             'products': products,
+            'basket': basket,
             }
 
     return render(request, 'mainapp/products.html', context)
 
+
 def contact(request):
-    file_path = settings.BASE_DIR / 'static' / 'data' / 'locations.json'
+    file_path = settings.BASE_DIR / 'static' / 'data'  /'locations.json'
     
     locations = []
 
