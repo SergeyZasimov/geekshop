3fe3a53428af0c7aedf7a6bd8b31c99159a2166a
SergeyZasimov <zasimov.ser@yandex.ru>
2021-04-21 14:14:10 +0300

    siege нагрузочное тестирование

--- shell_commands.sh
+++ shell_commands.sh
@@ -1,1 +1,7 @@
-sudo apt install graphviz
+# диаграмма классов проекта
+# sudo apt install graphviz
+
+# тестирование
+apt install siege
+siege -f /home/django/geekshop/urls_siege.txt -d1 -r10 -c1
+siege -f /home/django/geekshop/urls_siege.txt -d1 -r10 -c5
