8b4154f87dd02f49bb49760a641cbcbfc1215877
SergeyZasimov <zasimov.ser@yandex.ru>
2021-04-21 14:33:59 +0300

    Revert "siege нагрузочное тестирование со входом в учетную запись"
    This reverts commit 1d263451

--- shell_commands.sh
+++ shell_commands.sh
@@ -2,9 +2,6 @@
 # sudo apt install graphviz
 
 # тестирование
-#apt install siege
-#siege -f /home/django/geekshop/urls_siege.txt -d1 -r10 -c1
-#siege -f /home/django/geekshop/urls_siege.txt -d1 -r10 -c5
-
-# тестирование со входом в учетную запись
-siege -f /home/django/geekshop/urls_siege_login.txt -d1 -r10 -c1 --debug
+apt install siege
+siege -f /home/django/geekshop/urls_siege.txt -d1 -r10 -c1
+siege -f /home/django/geekshop/urls_siege.txt -d1 -r10 -c5
